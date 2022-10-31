da9e808b09bd249649d67ecb8a52233649db7033
Igor3k <Gothiquesxzz@gmail.com>
2022-10-31 15:51:39 +0700

    commit

--- scripts.js
+++ scripts.js
@@ -1,28 +1,52 @@
-const num1 = document.getElementById("num1");
+const calcWindow = document.getElementById('calcWindow');
 
-const num2 = document.getElementById("num2");
-
-const rezult = document.getElementById("rezult");
+const operatorWindow = document.getElementById('operatorWindow');
 
 let operator;
 
 const setOperator = (op) => {
     operator = op;
+    operatorWindow.textContent = operator;
 }
 
-calc = (num1, num2) => {
+let num1 = '';
+let num2 = '';
+
+const setNum = (num) => {
+    if (!operator) {
+        num1 += num;
+        calcWindow.textContent = num1;
+    } else {
+        num2 += num;
+        calcWindow.textContent = num2;
+    }
+}
+
+const reset = () => {
+    num1 = '';
+    num2 = '';
+    operator = null;
+    operatorWindow.textContent = '';
+    calcWindow.textContent = '';
+}
+
+const calc = (num1, num2) => {
     switch (operator) {
-        case 'plus':
-            rezult.textContent = num1 + num2;
+        case '+':
+            calcWindow.textContent = num1 + num2;
+            num2 = '';
             break;
-        case 'minus':
-            rezult.textContent = num1 - num2;
+        case '-':
+            calcWindow.textContent = num1 - num2;
+            num2 = '';
             break;
-        case 'mult':
-            rezult.textContent = num1 * num2;
+            case '*':
+            calcWindow.textContent = num1 * num2;
+            num2 = '';
             break;
-        case 'division':
-            rezult.textContent = num1 / num2;
+        case '/':
+            calcWindow.textContent = num1 / num2;
+            num2 = '';
             break;
     }
 }
